fd40698b3dd2f64dffd735f0e874209705fb26a8
Ruben <r.moronhinojosa@gmail.com>
2024-02-06 12:30:15 +0100

    Avance en slider y creación de footer

--- proyecto/src/app/app.routes.ts
+++ proyecto/src/app/app.routes.ts
@@ -2,5 +2,8 @@ import { Routes } from '@angular/router';
 import { IndexPagesComponent } from './pages/index-pages/index-pages.component';
 
 export const routes: Routes = [
-    
+    {
+        path: '',
+        component: IndexPagesComponent
+    }
 ];
--- proyecto/src/app/pages/index-pages/index-pages.component.ts
+++ proyecto/src/app/pages/index-pages/index-pages.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { SliderComponent } from '../../components/slider/slider.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-index-pages',
   standalone: true,
   imports: [HeaderComponent,
-  SliderComponent],
+    SliderComponent,
+    FooterComponent],
   templateUrl: './index-pages.component.html',
   styleUrl: './index-pages.component.css'
 })
