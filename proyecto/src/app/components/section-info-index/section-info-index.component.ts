be78b8599441c5e09028acc1d3a54200f287227f
Ruben <r.moronhinojosa@gmail.com>
2024-02-07 22:21:14 +0100

    Retoques footer, section-info

---
+++ proyecto/src/app/components/section-info-index/section-info-index.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-info-index',
+  standalone: true,
+  imports: [],
+  templateUrl: './section-info-index.component.html',
+  styleUrl: './section-info-index.component.css'
+})
+export class SectionInfoIndexComponent {
+
+}
