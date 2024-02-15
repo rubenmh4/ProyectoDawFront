c1d800781a2544e50daf88e61acd02ad7e17fc4f
Ruben <r.moronhinojosa@gmail.com>
2024-02-15 23:50:05 +0100

    avance pistasIndoorPages y creacion instalacionesPage

--- proyecto/src/app/app.routes.ts
+++ proyecto/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { IndexPagesComponent } from './pages/index-pages/index-pages.component';
 import { FormPagesComponent } from './pages/form-pages/form-pages.component';
 import { PistasIndoorPagesComponent } from './pages/pistas-indoor-pages/pistas-indoor-pages.component';
+import { InstalacionesPagesComponent } from './pages/instalaciones-pages/instalaciones-pages.component';
 
 export const routes: Routes = [
     {
@@ -16,5 +17,9 @@ export const routes: Routes = [
     {
         path:'pistasIndoor',
         component: PistasIndoorPagesComponent
+    },
+    {
+        path:'instalaciones',
+        component:InstalacionesPagesComponent
     }
 ];
