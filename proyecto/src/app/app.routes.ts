e61bbb990eeb1352cf2f98000607f83c892ac2ac
rubenmh4 <r.moronhinojosa@gmail.com>
2024-02-21 14:34:05 +0100

    modificaion routes, cambio de form por contacto y factorizacion

--- proyecto/src/app/app.routes.ts
+++ proyecto/src/app/app.routes.ts
@@ -11,7 +11,7 @@ export const routes: Routes = [
         pathMatch: 'full'
     },
     {
-        path: 'inicioSesion',
+        path: 'contacto',
         component: FormPagesComponent
     },
     {
