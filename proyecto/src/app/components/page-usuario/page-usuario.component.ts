28a03a23da0ae93d2b03600a1b334e6bcb30fcc1
Ruben <r.moronhinojosa@gmail.com>
2024-04-08 00:29:17 +0200

    Retoques en header, page-usuari y rutas de imagenes

--- proyecto/src/app/components/page-usuario/page-usuario.component.ts
+++ proyecto/src/app/components/page-usuario/page-usuario.component.ts
@@ -3,15 +3,25 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 import { EstadisticaUsuarioService } from '../../services/estadistica-usuario.service';
+import { RouterLink } from '@angular/router';
+
+
 @Component({
   selector: 'app-page-usuario',
   standalone: true,
-  imports: [HeaderComponent,FooterComponent],
+  imports: [HeaderComponent,FooterComponent,RouterLink],
   templateUrl: './page-usuario.component.html',
   styleUrl: './page-usuario.component.css'
 })
 export class PageUsuarioComponent {
-  posibilidadEditar :boolean = false;
+  posibilidadEditar :boolean = true;
 
+   usuario : {nombre:string,apellidos:string,lugarNac:string,fechaNac:number, email:string} = {
+    nombre : "Ruben",
+    apellidos : "Morón Hinojosa",
+    lugarNac : "Osuna",
+    fechaNac : 1,
+    email : "r.moronhinojosa@gmail.com"
+  }
 
 }
